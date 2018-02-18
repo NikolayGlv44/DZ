ceb6777f67b2c4b418aa2775d32be5b98eaacc5c
Nikolay Glebochev <nikolay.glv44@gmail.com>
2018-02-18 19:28:15 +0300

    Составьте функцию вычисления дискриминанта кв. ур.

---
+++ dz2/1_discrt.php
@@ -0,0 +1,31 @@
+<?php
+
+
+function discrt($a, $b, $c) {
+    return $b * $b - 4 * $a * $c;
+}
+
+$a = 1;
+$b = 2;
+$c = 0;
+
+echo $d = discrt($a, $b, $c);
+
+?><br><?php
+
+if ($d > 0) {
+    echo $x = (-$b + sqrt($d)) / (2*$a);
+    ?><br><?php
+    echo $x = (-$b - sqrt($d)) / (2*$a);
+} elseif ($d == 0){
+    echo $x = -$b / (2*$a);
+} else {
+    echo 'Нет корня';
+}
+
+?><br><?php
+
+
+?>
+
+
