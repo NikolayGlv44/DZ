d2e2a896a8398b64f14103e92ad9f3b846fda2bb
Nikolay Glebochev <nikolay.glv44@gmail.com>
2018-02-18 19:03:55 +0300

    dz 1.5

---
+++ dz1/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Test</title>
+</head>
+<body>
+
+    <h1>PHP</h1>
+
+    <?php
+
+    $a = 7;
+    $m = 10;
+    $c = 7;
+    $x = 2;
+    
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    $x = ($a * $x + $c) % $m;
+    echo $x;
+    ?><br><?php
+    ?>
+
+</body>
+</html>
