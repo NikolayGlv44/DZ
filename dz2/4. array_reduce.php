ac30130bb8fa92b44d2a7e22b181157879d6ce0b
Nikolay Glebochev <nikolay.glv44@gmail.com>
2018-02-22 12:05:16 +0300

    правка 2.2

--- dz2/4. array_reduce.php
+++ dz2/4. array_reduce.php
@@ -1,27 +1,31 @@
 
+
 <?php
 
 
 $name = ['Артём', 'Егор', 'Татьяна', 'Алексей', 'Сергей', 'Семён', 'Пётр', 'Андрей','Ёж'];
 
+
 foreach ($name as $valueName) {
     echo $valueName . ' ';
-
 }
+
+$str = implode($name);
 
 
 ?><br><?php
 
 
-$f = function ($carry, $item )
+$f = function ($carry, $item ) use ($str)
 {
-    $carry += (substr_count($item, 'Ё') + substr_count($item, 'ё')) / strlen($item);
+    $carry += (substr_count($item, 'Ё') + substr_count($item, 'ё')) / strlen($str);
     return $carry;
 };
 
 ?><br>Частота, с которой встречается буква 'Ё' в именах - <?php
 
 echo array_reduce($name, $f);
+
 ?>
 
 
